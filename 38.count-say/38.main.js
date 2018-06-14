c29823eebf251e71e70e0388b3c5fc480147f0b2
alexerhardt <alex.erhardt.es@gmail.com>
2018-06-14 14:08:41 +0200

    finish 38

--- 38.count-say/38.main.js
+++ 38.count-say/38.main.js
@@ -3,9 +3,34 @@
  * @return {string}
  */
 var countAndSay = function(n) {
-    if (n === 1) return "1"
+    if (n < 1) return "0";
 
-    return "0";
+    var result = "1";
+    for(var i = 1; i < n; i++) {
+        result = countString(result);
+    }
+
+    return result;
 };
+
+var countString = function(str) {
+    var arr = str.split("");
+
+    var resultString = "";
+    var currentChar = arr[0];
+    var charCount = 0;
+    for (var i = 0; i < arr.length; i++) {
+        charCount++;
+        if (i + 1 < arr.length && arr[i + 1] !== currentChar) {
+            resultString += charCount + currentChar;
+            charCount = 0; 
+            currentChar = arr[i + 1];
+        }
+    }
+    resultString += charCount + currentChar;
+    return resultString;
+};
+
+countAndSay(3);
 
 module.exports = countAndSay;
