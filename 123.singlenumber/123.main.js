01eeca285c56f456b8250e59e84ba5201043df70
alexerhardt <alex.erhardt.es@gmail.com>
2019-06-12 18:57:10 +0200

    finish 123

--- 123.singlenumber/123.main.js
+++ 123.singlenumber/123.main.js
@@ -1,5 +1,31 @@
-var singleNumber = function() {
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+  const map = {};
 
+  for (let i = 0; i < nums.length; i++) {
+    if (!map[nums[i]]) {
+      map[nums[i]] = 1;
+    }
+    else {
+      delete map[nums[i]];
+    }
+  }
+    
+  return new Number(Object.keys(map)[0]);
+    
 };
+
+// clever alternative
+var singleNumberClever = function(nums) {
+  let a = 0;
+  for (let num of nums) {
+    a ^= num;
+  }
+  return a;
+}
+
 
 module.exports = singleNumber;
